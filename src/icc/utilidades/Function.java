fc5348d4d45cb1043ba802e4c3730670cbee929a
Alvaro <a_90_hernandez@hotmail.com>
2020-11-12 15:48:43 -0600

    Función para convertir de decimal a binario

--- src/icc/utilidades/Function.java
+++ src/icc/utilidades/Function.java
@@ -7,25 +7,44 @@ package icc.utilidades;
  */
 public class Function {
     public boolean esPar(int numero) {
-        int residuo;
-
-        residuo = numero % 2;
-        if (residuo == 0) {
-          return true;
-        } else {
-          return false;
-        }
+        return numero % 2 == 0;
     }
 
     public boolean esImpar(int numero) {
-        int residuo;
+        return !this.esPar(numero);
+    }
 
-        residuo = numero % 2;
-        if (residuo == 1) {
-          return true;
+    /**
+     * Esta función devuelve la representación binaria de un numero entero en base 10
+     * @param numero : es el valor en base 10 para convertir a binario
+     * @return una cadena de 0s y 1s que corresponden a la representación binaria
+     */
+    public String convierteBinario(int numero) {
+        int numeroAuxiliar;
+        boolean esResiduoCero;
+        String signo;
+        String binario = "";
+
+        if (numero > 0) {
+            signo = "";
+            numeroAuxiliar = numero;
         } else {
-          return false;
+            signo = "-";
+            numeroAuxiliar = numero * -1;
         }
+
+        while (numeroAuxiliar != 1) {
+            if (this.esPar(numeroAuxiliar)) {
+                binario = 0 + binario;
+                numeroAuxiliar = numeroAuxiliar / 2;
+            } else {
+                binario = 1 + binario;
+                numeroAuxiliar = (numeroAuxiliar) / 2;
+            }
+        }
+        binario = signo + 1 + binario;
+
+        return binario;
     }
 
     public static void main(String[] args) {
@@ -34,5 +53,6 @@ public class Function {
       System.out.println(f.esPar(4));
       System.out.println(f.esImpar(5));
       System.out.println(f.esImpar(4));
+      System.out.println("La representación binaria de 45 es "+f.convierteBinario(45));
     }
 }
